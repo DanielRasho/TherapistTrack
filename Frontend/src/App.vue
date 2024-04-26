a865ba7aa1440e15ce001dbe6a2799df13e1a463
MarchMol <marchmol98@gmail.com>
2024-04-25 20:36:45 -0600

    FIX: now app is correctly modularized in vue components. Added local storage for token, got rid of default elements

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,24 +1,17 @@
 <template>
-  <div class="login-container">
-    <div class="login-form">
-      <h1>Login</h1>
-      <div class="form-group">
-        <label for="name">Nombres</label>
-        <input type="text" id="name" v-model="nombre" placeholder="Escribe tu nombre..." />
-      </div>
-      <div class="form-group">
-        <label for="password">Contraseña</label>
-        <input type="password" id="password" v-model="password" placeholder="••••••••" />
-        <span class="icon-eye"></span>
-      </div>
-      <button @click="ingresar">Ingresar →</button>
-      <p v-if="mensajeError" class="error-message">{{ mensajeError }}</p>
-    </div>
+  <div class="login-form">
+    <h1>Login</h1>
+    <Name />
+    <Password />
+    <button @click="ingresar">Ingresar →</button>
+    <p v-if="mensajeError" class="error-message">{{ mensajeError }}</p>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import Name from './components/Name.vue'
+import Password from './components/Password.vue'
 
 const nombre = ref('');
 const password = ref('');
@@ -30,46 +23,55 @@ async function ingresar() {
   if (nombre.value === '' || password.value === '') {
     mensajeError.value = 'Por favor, completa todos los campos.';
     return;
+  } else {
+    try {
+      loading.value = true;
+      const response = await fetch('https://tu-api.com/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: nombre.value,
+          password: password.value
+        })
+      });
+
+      if (!response.ok) {
+        throw new Error('Error en la solicitud de red');
+      }
+
+      const data = await response.json();
+
+      if (data.success) {
+        // Autenticación exitosa. Puedes guardar el token si viene uno, o redireccionar al usuario.
+        const token = data.token;
+        localStorage.setItem('token', token);
+
+      } else {
+        mensajeError.value = 'Nombre de usuario o contraseña incorrectos.';
+      }
+    } catch (error) {
+      mensajeError.value = 'Error al iniciar sesión: ' + error.message;
+    } finally {
+      loading.value = false;
+    }
   }
 
-  try {
-    loading.value = true;
-    const response = await fetch('https://tu-api.com/auth/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username: nombre.value,
-        password: password.value
-      })
-    });
 
-    if (!response.ok) {
-      throw new Error('Error en la solicitud de red');
-    }
-
-    const data = await response.json();
-    
-    if (data.success) {
-      // Autenticación exitosa. Puedes guardar el token si viene uno, o redireccionar al usuario.
-    } else {
-      mensajeError.value = 'Nombre de usuario o contraseña incorrectos.';
-    }
-  } catch (error) {
-    mensajeError.value = 'Error al iniciar sesión: ' + error.message;
-  } finally {
-    loading.value = false;
-  }
 }
 </script>
 
 <style>
-.login-container {
+body {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
+  width: 100vw;
+  background-color: #EDECE8;
+  margin: 0%;
+  padding: 0%;
 }
 
 .login-form {
@@ -77,61 +79,32 @@ async function ingresar() {
   padding: 2rem;
   border-radius: 1rem;
   box-shadow: 0 10px 25px rgba(0, 0, 0, 0.804);
-  width: 350px;
+  width: 390px;
   display: flex;
   flex-direction: column;
 }
 
 .login-form h1 {
   text-align: center;
+  color: black;
   margin-bottom: 2rem;
 }
 
-.login-button {
-  background: #ffc107;
+button {
+  background: #F9BB42;
   color: white;
   border: none;
   padding: .75rem 1.5rem;
   border-radius: .5rem;
   cursor: pointer;
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
-  margin-left: auto; 
-}
-
-.form-group {
-  margin-bottom: 1rem;
-}
-
-.form-group label {
-  display: block;
-  margin-bottom: .5rem;
-  color: black; 
-}
-
-.form-group input {
-  width: 100%;
-  padding: .75rem;
-  border: 1px solid #ddd;
-  border-radius: .5rem;
-  box-sizing: border-box;
-}
-
-.icon-eye {
-  position: absolute;
-  cursor: pointer;
-  right: 1rem;
-  top: 1rem; 
-}
-
-button {
-  background: #ffc107;
-  color: white; 
-  border: none;
-  padding: .75rem 1.5rem;
-  border-radius: .5rem;
-  cursor: pointer;
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
   align-self: flex-end;
+  transition: background-color 0.2s;
+}
+
+button:hover {
+
+  background: #FFCD6C;
 }
 
 .error-message {
---
+++ Frontend/src/components/Password.vue
@@ -0,0 +1,35 @@
+<template> 
+    <div class="form-group">
+        <label for="password">Contraseña</label>
+        <input type="password" id="password" v-model="password" placeholder="••••••••" maxlength="32"/>
+        <span class="icon-eye"></span>
+    </div>
+</template>
+
+<style>
+.form-group {
+  margin-bottom: 1rem;
+}
+
+.form-group label {
+  display: block;
+  margin-bottom: .5rem;
+  color: black; 
+}
+
+.form-group input {
+  width: 100%;
+  padding: .75rem;
+  border: 1px solid #ddd;
+  border-radius: .5rem;
+  box-sizing: border-box;
+}
+
+.icon-eye {
+  position: absolute;
+  cursor: pointer;
+  right: 1rem;
+  top: 1rem; 
+}
+
+</style>
