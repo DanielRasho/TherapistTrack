065205a5e61fb44b16c665db6d8aa20bc16165ef
MarchMol <marchmol98@gmail.com>
2024-05-10 01:49:06 -0600

    Feat: loading component for api fetching

---
+++ Frontend/src/components/Loading.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="center">
+        <img class="loading" src="../assets/loader-2-line.svg" alt="">
+    </div>
+    
+</template>
+
+<style>
+.center{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 5vh;
+}
+.loading{
+    height: 12vh;
+    animation: rotate 2.5s linear infinite;
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
