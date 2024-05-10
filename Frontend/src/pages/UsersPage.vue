79b5df5f405a376407e0a52175613182f2fc3e13
MarchMol <marchmol98@gmail.com>
2024-05-10 01:50:07 -0600

    Fix: infrastructure for api request, loading and simulated waiting time

--- Frontend/src/pages/UsersPage.vue
+++ Frontend/src/pages/UsersPage.vue
@@ -5,43 +5,96 @@
     <p>En esta vista puede administrar a los usuarios que tienen acceso a la aplicación y editar su información.
     </p><br>
     <div class="options">
-      <CustomInput :pholder="'Buscar por nombre'"/>
-      <Button :mssg="'Nuevo'" :onClick="clork"/>
+      <CustomInput :pholder="'Buscar por nombre'" />
+      <Button :mssg="'Nuevo'" :onClick="clork" />
     </div>
-
     <div class="grid-container">
-      <p class="grid-header">Nombre</p>
-      <p class="grid-header">Rol</p>
-        <div class="grid-item">Daniel Rayo</div>
-        <div class="grid-item">doctor</div>
+      <div class="grid-row">
+        <p class="grid-header">Nombre</p>
+        <p class="grid-header">Rol</p>
+      </div>
+      <div v-if="loading">
+        <Loading />
+      </div>
 
-
-        <div class="grid-item">Sofia de la Rosa</div>
-        <div class="grid-item">Doctor</div>
-
-        <div class="grid-item">Ricardo Morales Sagastume</div>
-        <div class="grid-item">Asistente</div>
+      <div class="grid-row" v-for="(item, key) in fetchedData" :key="key" @click="handleOpenView(key)">
+        <div class="grid-item">{{ item.nombre }}</div>
+        <div class="grid-item">{{ item.rol }}</div>
+      </div>
 
     </div>
   </div>
+  <div v-if="modalOpen">
+    <ViewUser :data=" fetchedData[selected] || {nombre: 'Rayo'}" @closeView="handleCloseView"/>
+  </div>
+
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, defineProps, onMounted } from 'vue';
 import '../assets/arrow-left-double-fill.svg'
 import '../components/Button.vue'
-import SideBar from'../components/SideBar.vue'
+import SideBar from '../components/SideBar.vue'
 import Button from '../components/Button.vue'
 import CustomInput from '../components/CustomInput.vue'
+import Loading from '../components/Loading.vue'
+import ViewUser from '../components/ViewUser.vue'
+
+const modalOpen = ref(false)
+const selected = ref(0)
+const fetchedData = ref(null);
+const loading = ref(false)
+
+onMounted(async () => {
+  // simulation time
+  loading.value = true
+  await new Promise(resolve => setTimeout(resolve, 5000));
+
+  // fetchData for when the backend gets deployed
+  // fetchedData.value = await fetchData(); 
+  fetchedData.value = {
+    1: {
+      nombre: 'Daniel Rayo',
+      rol: 'Doctor',
+      telefonos: ['555 555', '222 222'],
+      numColegiado: 32115,
+      correos: ['aaa@gmail.com', 'bbb@gmail.com']
+    },
+    2: {
+      nombre: 'Sofia de la Rosa',
+      rol: 'Doctor',
+      telefonos: ['444 444', '333 333'],
+      numColegiado: 53515,
+      correos: ['ccc@gmail.com', 'ddd@gmail.com']
+    },
+    3: {
+      nombre: 'Ricardo Morales Sagastume',
+      rol: 'Asistente',
+      telefonos: ['111 111', '777 777'],
+      numColegiado: null,
+      correos: ['eee@gmail.com']
+    }
+  }
+  loading.value = false
+  return fetchedData
+});
 
 const clork = () => {
   console.log("Button pressed")
 }
 
+const handleCloseView = () => {
+  modalOpen.value=false
+}
+
+const handleOpenView = (key) => {
+  selected.value = key
+  modalOpen.value=true
+}
+
 </script>
 
 <style>
-
 #app {
   height: 100vh;
   width: 100vw;
@@ -58,7 +111,7 @@ const clork = () => {
   padding-top: 3rem;
 }
 
-.options{
+.options {
   display: flex;
   width: 100%;
   justify-content: space-between;
@@ -71,7 +124,7 @@ ul {
   color: black;
 }
 
-p {
+.page p {
   font-weight: lighter;
   font-size: small;
   color: var(--vt-c-dark-gray-1)
@@ -80,29 +133,34 @@ p {
 
 .grid-container {
   display: grid;
-  grid-template-columns: auto auto;
+  grid-template-columns: auto;
   /* Two columns */
   color: var(--vt-c-dark-1);
   padding: 1rem;
 }
 
-.grid-header,
-.grid-item {
+.grid-row {
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+.page .grid-header,
+.page .grid-item {
   padding: 0.5rem;
   border-bottom: 0.2vh solid #ccc;
 }
 
-.grid-header {
+.page  .grid-header {
   font-family: 'MotivaSansLighter';
   border-top: 0.2vh solid #ccc;
   font-weight: bold;
 }
 
-.grid-item :nth-child(even) {
+.page .grid-item+.grid-item {
   border-left: 1px solid #ccc;
 }
-
-.grid-item:nth-child(odd):hover + .grid-item,.grid-item:nth-child(odd):hover{
+.page  .grid-row:hover {
   background-color: var(--vt-c-light-yellow-1);
   cursor: pointer;
 }
